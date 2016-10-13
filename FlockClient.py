0f60822b8b9d233ec9d1e3b4fcd68bc424336df2
Balajiganapathi S <balajiganapathi.s@gmail.com>
2016-10-13 18:23:37 +0530

    Add send chat api

--- FlockClient.py
+++ FlockClient.py
@@ -1,36 +1,53 @@
 import requests, json
 
-class FlockClient:
-    _user_token = None
-    _api_url = "https://api.flock.co/v1/"
-    _app_id
+class FlockClient(object):
+    _api_url = "https://api.flock-staging.co/v1/"
+    _token = None
+    _app_id = None
 
-    def _init__(self, user_token, app_id):
-        if not user_token:
-            raise ValueError("user_token is required")
-        if not app_id:
+    def __init__(self, token, app_id):
+        if token is None:
+            raise ValueError("token is required")
+        if app_id is None:
             raise ValueError("app_id is required")
-        self._user_token = user_token
+        self._token = token
+        self._app_id = app_id
+
+    def send_chat(self, message):
+        data = self._dict_from_message(message)
+        r = self._post_request(data, "chat.sendMessage")
+        return r.text
+
+#----------------------------Internal functions---------------------------------
+    def _post_request(self, data, endpoint):
+        data['token'] = self._token
+        data['appid'] = self._app_id
+        r = requests.post(self._get_url(endpoint), params=data)
+        print r
+        return r
 
     def _get_url(self, endpoint):
         return self._api_url + endpoint
 
-    def send_chat(self, message):
-        data = self._dict_from_message(message)
-        data['token'] = user_token
-        r = requests.post(self._get_url("chat.sendMessage"), )
-        return r.text
-
-    def _dict_from_message(message):
+    def _dict_from_message(self, message):
         data = {}
-        data['from'] = message.from_guid
+        if not message.from_guid is None:
+            data['from'] = message.from_guid
         data['to'] = message.to_guid
         data['text'] = message.text
         data['attachments'] = json.dumps(message.attachments)
-        data['sendAs'] = json.dumps(message.send_as)
+        if not message.send_as is None:
+            data['sendAs'] = json.dumps(message.send_as)
         data['flockml'] = message.flockml
         data['notification'] = message.notification
         data['mentions'] = json.dumps(message.mentions)
 
         return data
+
+#----------------------------Standard functions-----------------------------------
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return "FlockClient[token=%s, app_id=%s" % (self._token, self._app_id)
 
--- Message.py
+++ Message.py
@@ -1,4 +1,4 @@
-class Message:
+class Message(object):
     to_guid = None
     from_guid = None
     text = None
@@ -8,6 +8,13 @@ class Message:
     notification = None
     mentions = None
 
-    def __init__(self, to_guid = None, from_guid = None, text = "", attachments = [], send_as = None, flockml = "", notification = "", mentions = []):
-        if not (to_guid  || from_guid 
+    def __init__(self, to_guid = None, from_guid = None, text = "", attachments = [], send_as = {}, flockml = "", notification = "", mentions = []):
+        self.to_guid = to_guid
+        self.from_guid = from_guid
+        self.text = text
+        self.attachments = attachments
+        self.send_as = send_as
+        self.flockml = flockml
+        self.notification = notification
+        self.mentions = mentions
 
