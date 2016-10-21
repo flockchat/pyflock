2c6cea9aad3880c5cf3dbf5f8b6d05fbf32c7317
Balajiganapathi S <balajiganapathi.s@gmail.com>
2016-10-21 20:16:27 +0530

    Rename user1 to user

--- sample.py
+++ sample.py
@@ -8,7 +8,7 @@ from pyflock import Message, SendAs, Attachment, Views, WidgetView, HtmlView, Im
 
 # The test ids and tokens
 bot_token = 'd4c44699-a1ee-457f-bb31-e444656051b2'
-user1_guid = 'u:7v3ni473vei3nnbi'
+user_guid = 'u:7v3ni473vei3nnbi'
 user2_guid = 'u:5qqt9oyaotyrroa5'
 group_id = 'g:2213136127433909016'
 app_id = '987b522b-c1b8-41a4-9d22-5f981c464fef'
@@ -20,14 +20,14 @@ flock_client = FlockClient(token=bot_token, app_id=app_id)
 res = None
 
 # Send a simple send message
-simple_message = Message(to=user1_guid,text="Hello, world")
+simple_message = Message(to=user_guid,text="Hello, world")
 # returns a message id
 res = flock_client.send_chat(simple_message)
 print(res)
 
 # Send a message using a custom name and profile image
 send_as_hal = SendAs(name='HAL-9000', profile_image='https://pbs.twimg.com/profile_images/1788506913/HAL-MC2_400x400.png')
-send_as_message = Message(to=user1_guid,text="I'm sorry Dave, I'm afraid I can't do that",send_as=send_as_hal)
+send_as_message = Message(to=user_guid,text="I'm sorry Dave, I'm afraid I can't do that",send_as=send_as_hal)
 res = flock_client.send_chat(send_as_message)
 print(res)
 
@@ -38,7 +38,7 @@ views.add_widget(widget)
 
 attachment = Attachment(title="Test widget", description="Replace src with your own page", views=views)
 # NOTE: attachments is an array of attachment
-widget_message = Message(to=user1_guid, attachments = [attachment])
+widget_message = Message(to=user_guid, attachments = [attachment])
 res = flock_client.send_chat(widget_message)
 print(res)
 
@@ -49,7 +49,7 @@ views.add_html(html)
 
 attachment = Attachment(title="Test html", description="Replace inline with your own html", views=views)
 # NOTE: attachments is an array of attachment
-html_message = Message(to=user1_guid, attachments = [attachment])
+html_message = Message(to=user_guid, attachments = [attachment])
 res = flock_client.send_chat(html_message)
 print(res)
 
@@ -60,7 +60,7 @@ views.add_flockml("<flockml>FlockML is <b>AWESOME</b></flockml>")
 
 attachment = Attachment(title="Test flockml", description="Replace flockml with your own flockml", views=views)
 # NOTE: attachments is an array of attachment
-flockml_message = Message(to=user1_guid, attachments = [attachment])
+flockml_message = Message(to=user_guid, attachments = [attachment])
 res = flock_client.send_chat(flockml_message)
 print(res)
 
@@ -71,7 +71,7 @@ views.add_image(image)
 
 attachment = Attachment(title="Test image", description="One Ring to rule them all, One Ring to find them, One Ring to bring them all and in the darkness bind them", views=views)
 # NOTE: attachments is an array of attachment
-image_message = Message(to=user1_guid, attachments = [attachment])
+image_message = Message(to=user_guid, attachments = [attachment])
 res = flock_client.send_chat(image_message)
 print(res)
 
@@ -83,7 +83,7 @@ views.add_flockml("<flockml>Download the <i>matrix</i></flockml>")
 
 # NOTE: downloads is always a list
 attachment = Attachment(title="Test files", downloads=[d], views=views)
-files_message = Message(to=user1_guid, attachments = [attachment])
+files_message = Message(to=user_guid, attachments = [attachment])
 res = flock_client.send_chat(files_message)
 print(res)
 
@@ -92,13 +92,13 @@ b1 = Button(name = "Harry Potter", id="harry", action=OpenWidgetAction(url="http
 b2 = Button(name = "Ron Weasley", id="ron", action=OpenBrowserAction(url="https://goo.gl/gDpMVn", send_context=True))
 b3 = Button(name = "Hermione Granger", id="hermione", action=SendToAppAction())
 attachment = Attachment(title="Test buttons", buttons=[b1,b2,b3])
-button_message = Message(to=user1_guid, text="Who is your favourite Harry Potter character?", attachments = [attachment])
+button_message = Message(to=user_guid, text="Who is your favourite Harry Potter character?", attachments = [attachment])
 res = flock_client.send_chat(button_message)
 print(res)
 
 # Now, just for fun, let us change colours
 attachment = Attachment(title="Test colour", color="#FF0000", description="It is red!")
-color_message = Message(to=user1_guid, attachments=[attachment])
+color_message = Message(to=user_guid, attachments=[attachment])
 res = flock_client.send_chat(color_message)
 print(res)
 
